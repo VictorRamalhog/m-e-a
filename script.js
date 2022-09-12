12005c8e766e7e9787cd16ca7a4a60421130566c
Victor Ramalho <89925990+VictorRamalhog@users.noreply.github.com>
2022-09-12 11:04:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("abrir").onclick =()=>{
+    document.getElementById
+("menu").style.left="0px";
+
+}
+
+document.getElementById("fechar").onclick=()=>{
+    document.getElementById
+("menu").style.left="-200px";
+
+}
